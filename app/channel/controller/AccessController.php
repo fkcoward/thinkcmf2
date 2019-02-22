39a9c2a623353580303aa9afd10296d1f5144f80
liu <liu@qq.com>
2019-02-22 17:21:34 +0800

    加入一天内的按小时的统计图

--- app/channel/controller/AccessController.php
+++ app/channel/controller/AccessController.php
@@ -48,16 +48,52 @@ class AccessController extends AdminBaseController
         });
         $y_data = [];
         $data = [];
-        $bing_data=[];
-        foreach ($list as $key=>$value) {
-            $y_data[]=$value['path_info'];
-            $data[]=$value['num'];
-            $bing_data[]=['name'=>$value['path_info'],'value'=>$value['num']];
+        $bing_data = [];
+        foreach ($list as $key => $value) {
+            $y_data[] = $value['path_info'];
+            $data[] = $value['num'];
+            $bing_data[] = ['name' => $value['path_info'], 'value' => $value['num']];
         }
-        $this->assign('height',$list->count()*50);
-        $this->assign('y_data',json_encode($y_data));
-        $this->assign('data',json_encode($data));
-        $this->assign('bing_data',json_encode($bing_data));
+        $this->assign('height', $list->count() * 50);
+        $this->assign('y_data', json_encode($y_data));
+        $this->assign('data', json_encode($data));
+        $this->assign('bing_data', json_encode($bing_data));
         return $this->fetch();
+    }
+
+    public function lineChartHours()
+    {
+        $arr=$this->lineChart("hours");
+        $this->assign("keys",json_encode(array_keys($arr)));
+        $this->assign("values",json_encode(array_values($arr)));
+        return $this->fetch();
+    }
+
+    private function lineChart($type = "hours")
+    {
+        if (strtolower($type) == "hours") {
+            $limitTime = strtotime("-1 day");
+        }
+        $list = Db::name("access_log_count")->where('begin', '>=', $limitTime)->cache(600)->group("begin")->field("begin,sum(num) as num")->order("begin asc")->select();
+        $list2 = [];
+        foreach ($list as $value) {
+            $list2[$value['begin']] = $value['num'];
+        }
+        $data_arr = [];
+        $theTime = time();
+        while ($limitTime < $theTime) {
+            $h = date("H", $limitTime);
+            $data_arr[$h] = 0;
+            foreach ($list2 as $key => $val) {
+                if ($key >= $limitTime && $key < $limitTime + 3600) {
+                    $data_arr[$h] += empty($val) ? 0 : $val;
+                } else if ($key >= $limitTime + 3600) {
+                    break;
+                }
+            }
+
+            $limitTime += 3600;
+        }
+        return $data_arr;
     }
 }
