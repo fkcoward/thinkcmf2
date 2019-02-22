aa07d929fb45ee9cf19fd83de28250cd650a8530
liu <liu@qq.com>
2019-02-22 10:06:02 +0800

    加入一天内的统计图

--- app/channel/controller/AccessController.php
+++ app/channel/controller/AccessController.php
@@ -35,4 +35,29 @@ class AccessController extends AdminBaseController
         // 渲染模板输出
         return $this->fetch();
     }
+
+    public function countTodayCharts()
+    {
+        $beginTime = strtotime("-1 day");
+        $list = Db::name("access_log_count")->where('begin', '<', $beginTime)->cache(600)->group("path_info")->field("path_info,sum(num) as num")->order("num asc")->select();
+        $channle = Db::name('channel')->cache(true)->column("nickname", 'name');
+
+        $list->each(function ($item, $key) use ($channle) {
+            $item['path_info'] = empty($channle[$item['path_info']]) ? '' : $channle[$item['path_info']];
+            return $item;
+        });
+        $y_data = [];
+        $data = [];
+        $bing_data=[];
+        foreach ($list as $key=>$value) {
+            $y_data[]=$value['path_info'];
+            $data[]=$value['num'];
+            $bing_data[]=['name'=>$value['path_info'],'value'=>$value['num']];
+        }
+        $this->assign('height',$list->count()*50);
+        $this->assign('y_data',json_encode($y_data));
+        $this->assign('data',json_encode($data));
+        $this->assign('bing_data',json_encode($bing_data));
+        return $this->fetch();
+    }
 }
